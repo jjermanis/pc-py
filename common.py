f538d45e9d370dfae4e210459ffeb6db2fae4ccd
John Jermanis <33067902+jjermanis@users.noreply.github.com>
2017-12-07 09:24:57 -0800

    Solution to challenge 13

--- common.py
+++ common.py
@@ -29,6 +29,15 @@ def download_file_if_necessary(url, local_path):
         urllib.request.urlretrieve(url, local_path)
 
 
+def does_file_exist(filename, challenge_name):
+    try:
+        with open(filename, "r") as _:
+            return True
+    except FileNotFoundError:
+        print(f"Please download {filename} from {challenge_name} and save to this local folder.")
+        return False
+
+
 def authentication_help():
     print("You may be prompted for simple auth credentials.  Here they are if you need them.")
     username, password = get_credentials()
